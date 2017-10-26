a474e6b979046d67710b3cd86a8c720ef665faa0
sjfricke <sjfricke@wisc.edu>
2017-10-26 16:49:32 -0500

    finished tempalte

--- javascript/audioVisualizer.js
+++ javascript/audioVisualizer.js
@@ -3,74 +3,24 @@ var meshArray = [];
 
 function init() {
 
-    scene = new THREE.Scene();
-
-    renderer = new THREE.WebGLRenderer();
-    renderer.setPixelRatio( window.devicePixelRatio );
-    renderer.setSize( window.innerWidth, window.innerHeight );
-
-    document.body.appendChild( renderer.domElement );
-
-    camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 1000 );
-    camera.position.z = 500;
-    controls = new THREE.OrbitControls( camera, renderer.domElement );
-    controls.addEventListener( 'change', render ); // remove when using animation loop
-    controls.enableZoom = false;
-
-    var geometry = new THREE.BoxGeometry( 10, 10, 10 );
-    var material = new THREE.MeshLambertMaterial( { color: 0x30D0FF } );
-
-    for (i = 0; i < 64; i++) {
-        meshArray.push(new THREE.Mesh( geometry, material ));
-        meshArray[i].position.x = (i - 50) * 15;
-        meshArray[i].updateMatrix();
-        scene.add( meshArray[i] );
-    }
-
-
-    var dirLight = new THREE.DirectionalLight( 0xffffff );
-    dirLight.position.set( 1, 1, 1 );
-    scene.add( dirLight );
-
-    var AmbLight = new THREE.AmbientLight( 0x222222 );
-    scene.add( AmbLight );
-
-    
-    window.addEventListener( 'resize', onWindowResize, false );
-
-    setupAudio();
-
-    animate();
 }
 
 function onWindowResize() {
-
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-
-    renderer.setSize( window.innerWidth, window.innerHeight );
+    
 }
 
 function animate() {
 
-    requestAnimationFrame( animate );
-
-    
-    analyser.getByteFrequencyData(frequencyData);
-    for (i = 0; i < 64; i++) {
-        meshArray[i].scale.y = Math.max(1, frequencyData[i]) / 10;
-    }
-
-    controls.update();
-
-    render();
 }
 
 function render() {
     renderer.render( scene, camera );
 }
 
+// All audio functionallity will be taken care when setupAudio() is called
+// The function starts playing the audio and has all data saved to frequencyData
 var audioCtx, analyser, audioDiv, audioSrc, frequencyData;
+
 function setupAudio() {
     audioCtx = new(window.AudioContext || window.webkitAudioContext)();
     analyser = audioCtx.createAnalyser();
